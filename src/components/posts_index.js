7eccc18944fb155120686a61d1eff24a6e034c41
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-04-16 07:52:59 +0545

    Upto Navigation using Link

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,6 +2,7 @@ import React,{Component} from "react";
 import {connect} from "react-redux";
 import {fetchPosts} from "../actions";
 import _ from "lodash";
+import {Link} from "react-router-dom";
 
 class PostsIndex extends Component{
 
@@ -22,7 +23,14 @@ class PostsIndex extends Component{
     render(){
         return(
             <div>
-                <h3>    Posts Index </h3>
+
+                <div className="text-xs-right">
+                        <Link className="btn btn-primary" to="/posts/new">
+                            Add a Post
+                         </Link>
+                </div>
+                <h3> Posts Index </h3>
+
 
                 <ul className="list-group">
                         {this.renderPosts()}
---
+++ src/components/posts_new.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class PostsNew extends Component{
+
+
+    render(){
+        return(
+            <div>
+                    PostsNew
+
+            </div>
+        )
+    }
+
+
+}
+
+export default PostsNew;

89e9bc443aef68ca4c5cb4b1f0e25add415b070b
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-04-21 19:14:04 +0545

    Showing errors while validating form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -9,21 +9,25 @@ class PostsNew extends Component{
                 <div className="form-group">
                         <label>{field.label} </label>
                         <input className="form-control" type="text" {...field.input}  />
-                {field.meta.error}
-                
+                               {field.meta.touched?  field.meta.error : ""}
                         </div>
             )
     }
 
-
+onSubmit(values){
+        console.log("values :: ", values);
+}
   
     render(){
+        const {handleSubmit} = this.props;
         return(
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                     <Field label="TITLE" name="title" component={this.renderField} />
                     <Field  label="CATEGORIES" name="categories" component={this.renderField} />
                     <Field  label="POST CONTENT" name="content" component={this.renderField} />
-            </form>
+            
+            <button type="submit" className="btn btn-primary">Submit </button>
+                    </form>
         )
     }
 
@@ -33,9 +37,9 @@ class PostsNew extends Component{
 function validate(values){
     const errors={};
 
-    if(values.title.length<3){
-        errors.title ="Title must be at least 3 character ";
-    }
+    // if(values.title.length<3){
+    //     errors.title ="Title must be at least 3 character ";
+    // }
 
         if(!values.title ){
             errors.title ="Enter a title!";
